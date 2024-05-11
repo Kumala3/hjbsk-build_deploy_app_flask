7b290e9169b4142ac6074992ffb7d8f1dfa048f3
Kostek <ksytnik73kwer@gmail.com>
2024-05-11 22:01:19 +0200

    Refactor server.py to create MathOperations class for calculations in mathematics.py

--- maths/mathematics.py
+++ maths/mathematics.py
@@ -1,10 +1,9 @@
-def summation(a: float, b: float):
-    return a + b
+class MathOperations:
+    def summation(self, a: float, b: float):
+        return a + b
 
+    def subtraction(self, a: float, b: float):
+        return a - b
 
-def subtraction(a: float, b: float):
-    return a - b
-
-
-def multiplication(a: float, b: float):
-    return a * b
+    def multiplication(self, a: float, b: float):
+        return a * b
--- server.py
+++ server.py
@@ -1,37 +1,46 @@
+import os
+
 from flask import Flask, render_template, request
-from maths.mathematics import summation, subtraction, multiplication
+from maths.mathematics import MathOperations
+
+os.chdir("E:\COURSERA\PythonFlask\week_2\hjbsk-build_deploy_app_flask")
 
 app = Flask("Mathematics Problem Solver")
-
-@app.route("/sum")
-def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-
-    result = summation(num1, num2)
-    return f"The sum of {num1} and {num2} is {result}"
-
-@app.route("/sub")
-def sub_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    
-    result = subtraction(num1, num2)
-    return f"The difference of {num1} and {num2} is {result}"
-
-
-@app.route("/mul")
-def mul_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    
-    result = multiplication(num1, num2)
-    return f"The product of {num1} and {num2} is {result}"
 
 
 @app.route("/")
 def render_index_page():
     return render_template("index.html")
+
+
+@app.route("/sum")
+def sum_route():
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+
+    math_solver = MathOperations()
+    result = math_solver.summation(num1, num2)
+    return f"The sum of {num1} and {num2} is {result}"
+
+
+@app.route("/sub")
+def sub_route():
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+
+    math_solver = MathOperations()
+    result = math_solver.subtraction(num1, num2)
+    return f"The difference of {num1} and {num2} is {result}"
+
+
+@app.route("/mul")
+def mul_route():
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+
+    math_solver = MathOperations()
+    result = math_solver.multiplication(num1, num2)
+    return f"The product of {num1} and {num2} is {result}"
 
 
 if __name__ == "__main__":
